d7dfe6db261d59aa04df789a40a9bc8a38be9548
jyx <1449843302@qq.com>
2017-12-20 22:27:02 +0800

    modulize

--- src/qm.ts
+++ src/qm.ts
@@ -1,4 +1,4 @@
-import {staffUnit} from './staffUnit.js'
+import {qysFileParser} from './qysFileParser.js'
 
 function play(file : File){
     let reader : FileReader = new FileReader()
@@ -10,37 +10,9 @@ function play(file : File){
 }
 
 function parseFile(content : string){
-    let parser = new qyParser(content)
+    let parser = new qysFileParser(content)
     parser.parse()
 }
 
-class qyParser {
-    private _rawContent: string;
-    private length: number;
-    result: Array<staffUnit>;
-    
-    get rawContent(): string {
-        return this._rawContent;
-    }
 
-    set rawContent(content: string) {
-    }
-
-    constructor(content) {
-        this._rawContent = content
-        this.length = this._rawContent.length
-    }
-
-    parse(){
-        for (let i=0;i < this.length;i++) {
-            let staff = new staffUnit()
-            staff.staff = this.getChar(i)
-            this.result.push(staff)
-        }
-    }
-
-    getChar(index : number){
-        return this._rawContent.charAt(index)        
-    }
-}
 
